418025a0a67609fd7bae2b997322ba9d45e3e7b2
EdenOren77 <edenzoe.oren@live.biu.ac.il>
2025-12-13 20:15:34 +0200

    restore standard 3-channel RGB for ResNet50 compatibility

--- src/model.py
+++ src/model.py
@@ -8,15 +8,15 @@ class BreastCancerModel(nn.Module):
 
         self.resnet=models.resnet50(weights='IMAGENET1K_V1') #IMAGENET1K_V1- Loads trained weights    
         
-        #Adjusting the entrance to the first layer (only the in channel to 1)
-        self.resnet.conv1=nn.Conv2d(
-            in_channels=1,
-            out_channels=64,
-            kernel_size=7,
-            stride=2,
-            padding=3,
-            bias=False
-        )
+        # #Adjusting the entrance to the first layer (only the in channel to 1)
+        # self.resnet.conv1=nn.Conv2d(
+        #     in_channels=1,
+        #     out_channels=64,
+        #     kernel_size=7,
+        #     stride=2,
+        #     padding=3,
+        #     bias=False
+        # )
 
         #Adjusting the fc layer to 3
         num_features=self.resnet.fc.in_features
